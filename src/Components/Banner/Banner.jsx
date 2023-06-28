5d4f43124fd6b35e3609d89b011389805b1840ec
AhaD <abuahad35188@gmail.com>
2023-06-28 13:53:23 +0600

    banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,27 @@
-import './Banner.css'
 import { FaFacebookF,FaLinkedinIn,FaInstagram} from "react-icons/fa";
-
-import img from '../../../public/IMG_3629.jpg'
+import img from '../../../src/assets/Banner.jpg'
+import pdf from '../../../public/AbuAhad-Resume.pdf'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './Banner.css'
+import { useEffect } from "react";
 const Banner = () => {
+     useEffect(()=>{AOS.init();},[])
      return (
           <section className="home" id="home">
-          <div className="home-t">
+          <div className="home-t" data-aos="fade-right" data-aos-duration='1500'>
                <h3>Hello I,am</h3>
                <h1>Abu <span>Ahad</span></h1>
                <h5> Creative web <span>Developer</span></h5>
-               <p>I'm creative designer based in New York, and I'm very passionate and dedicated to my work.</p>
+               <p>I'm creative  web Developer and Designer. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam omnis commodi harum consequuntur nesciunt atque maxime ab alias cupiditate doloremque, fugit et quis quaerat ducimus, non eveniet officiis veritatis nulla! </p>
                 <div className="social">
                     <a href=""><FaFacebookF></FaFacebookF></a>
                     <a href=""><FaLinkedinIn></FaLinkedinIn></a>
                     <a href=""><FaInstagram></FaInstagram></a>
                 </div>
-                    <a href="#" className="btn">About Me</a> 
+                    <a href={pdf} download={pdf} className="btn-d">Download-Resume</a> 
           </div>
-          <div className="home-img">
-               {/* <img src="../../assets/banner.JPG" alt="" width="550"/> */}
+          <div className="home-img" data-aos="fade-left" data-aos-duration='3000'>
                <img src={img} alt="" />
           </div>
      </section>
--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-        <a href="https://drive.google.com/file/d/1rN_6PleWlTO5NLZXks_YacpL7Eqidgzr/view?usp=sharing" target="/_blank" className="top-btn">Download-Resume</a>
+        <a href="https://drive.google.com/file/d/1rN_6PleWlTO5NLZXks_YacpL7Eqidgzr/view?usp=sharing" target="/_blank" className="top-btn">Resume</a>
         </div>
       </div>
 
@@ -56,17 +56,4 @@ const Navbar = () => {
 
 export default Navbar;
 
-{
-  /* <header>
-<a href="#" className="logo">Bin<span>Das!</span></a>
- <div className="bx bx-menu menu-icon" id="menu-icon"> <FaBars/></div>
- <ul className="nav">
-     <li><a href="#home">Home</a></li>
-     <li><a href="#about">About</a></li>
-     <li><a href="#portfolio">Portfolio</a></li>
-     <li><a href="#services">Services</a></li>
-     <li><a href="#contact">Contact</a></li>
- </ul>
- <a href="https://drive.google.com/file/d/1rN_6PleWlTO5NLZXks_YacpL7Eqidgzr/view?usp=sharing" target="/_blank" className="top-btn">Download-Cv</a>
-</header> */
-}
+ 
