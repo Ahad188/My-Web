c6f04ea26ed4c9a1edb4921f998d28fec6600524
AhaD <abuahad35188@gmail.com>
2023-09-28 12:56:56 +0600

    skill section update

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
            
      
   return (
-    <div className="">
+    <div className="container">
       {/*  */}
       <div className="navbar bg-base-100">
         <div className="navbar-start">
--- src/Components/Skill/Skill.jsx
+++ src/Components/Skill/Skill.jsx
@@ -1,5 +1,15 @@
 import './Skill.css'
-
+import html from "../../assets/skill/html.png"  
+import css from "../../assets/skill/css.png"  
+import js from "../../assets/skill/javascript.png"  
+import boos from "../../assets/skill/bootstrap.png"  
+import tal from "../../assets/skill/tailwind-css.png"  
+import react from "../../assets/skill/Why is React so popular as a JavaScript library_.jpeg"  
+import fire from "../../assets/skill/firebase.png"  
+import mongo from "../../assets/skill/mongodb.png"  
+import ex from "../../assets/skill/express.png"  
+import git from "../../assets/skill/github.png"  
+import node from "../../assets/skill/nodejs.png" 
 const Skill = () => {
      return (
           <section className="w-[100%] mx-auto" id="skill" data-aos="zoom-in-down">
@@ -8,54 +18,42 @@ const Skill = () => {
                <div className="">
             <h1 className="title-text"> My Skill</h1>
 
-            <div className="skill-box">
-                <span className="title">HTML</span>
-                <div className="skill-bar">
-                    <span className="skill-per html">
-                        <span className="tooltip">95%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">CSS</span>
-                <div className="skill-bar">
-                    <span className="skill-per css">
-                        <span className="tooltip">80%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">JavaScript</span>
-                <div className="skill-bar">
-                    <span className="skill-per javascript">
-                        <span className="tooltip">60%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">NodeJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per nodejs">
-                        <span className="tooltip">40%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">ReactJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per react-js">
-                        <span className="tooltip">70%</span>
-                    </span>
-                </div>
-            </div>
-            <div className="skill-box">
-                <span className="title">ExpressJS</span>
-                <div className="skill-bar">
-                    <span className="skill-per express-js">
-                        <span className="tooltip">75%</span>
-                    </span>
-                </div>
-            </div>
+            <div className="big">
+                    <div className="box" data-aos="flip-left">
+                         <img src={html} title="Html" alt="" />
+                          
+                    </div>
+                    <div className="box" data-aos="flip-left">
+                         <img src={css} title="css" alt="" />
+                    </div>
+                    <div className="box" data-aos="zoom-in">
+                         <img src={js} title="Javascript"alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-right">
+                         <img src={boos} title="Bootstrap" />
+                    </div>
+                    <div className="box" data-aos="flip-right">
+                         <img src={tal} title=" Tailwind css"alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-left">
+                         <img src={react} title="React" alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-left">
+                         <img src={fire} title="Firebox" alt="Firebox" />
+                    </div>
+                    <div className="box" data-aos="zoom-in">
+                         <img src={mongo} title="MongoDB" alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-right">
+                         <img src={ex} title="Express" alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-right">
+                         <img src={git} title="Git" alt="" />
+                    </div>
+                    <div className="box" data-aos="flip-left">
+                         <img src={node} title="Node" alt="" />
+                    </div>
+               </div>
         </div>
                {/*  */}
                 
